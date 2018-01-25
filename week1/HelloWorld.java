be816dbf8fce6e1f3c2ffae46438126aec59ba97
Liam Mc Cormick <ljmc2000@gmail.com>
2018-01-25 11:16:23 +0000

    Well I just wrote my first java programme. which I had written several times before and still forgot the String[] args in the main method. this language is a pain

---
+++ week1/HelloWorld.java
@@ -0,0 +1,17 @@
+/*
+Name: Hello world
+Description: Print the words Hello world to the console
+Author: Liam McCormick
+Date: 25-01-18
+OS: Arch Linux
+Kernel: linux 4.14.14-1-ARCH
+Java version: 1.8.0_162
+*/
+
+public class HelloWorld
+{
+	public static void main(String[] args) //the s in string is case sensitive and must be capital
+	{
+		System.out.println("Hello World");
+	}
+}
