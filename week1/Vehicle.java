71f179c48daf70cfdfd0903bb6a232b8a458d23f
Liam Mc Cormick <ljmc2000@gmail.com>
2018-01-25 11:36:09 +0000

    added Vehicle.java: a class to represent a vehicle

---
+++ week1/Vehicle.java
@@ -0,0 +1,31 @@
+/*
+Class Description: A class to represent a vehicle
+Author: Liam McCormick
+Date: 25-01-18
+OS: Arch Linux
+Kernel: 4.14.14-1-ARCH
+Java Version: 1.8.0_162
+*/
+
+package org.lab1.test;	//put this class in the same package as hello world
+
+public class Vehicle
+{
+	String ownerName;
+	String regNumber;
+	String maxSpeed;
+	String colour;
+	bool automatic;
+	int numberOfWheels;
+
+	//constructer for vehicle
+	Public Vehicle(String owner, String reg, String topSpeed, String colour, bool automatic, int wheels) //constructer
+	{
+		self.ownerName=owner;
+		self.regNumber=reg;
+		self.topSpeed=maxSpeed;
+		self.colour=colour;
+		self.automatic=automatic;
+		self.numberOfWheels=wheels;
+	}//end constructer or vehicle
+}
