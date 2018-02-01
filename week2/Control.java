2d0897c3fc5a3f0c0cf81e06acfee0b83668b955
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-01 11:48:36 +0000

    well the Animal class works, but the output looks dumb if you use just initialise the name

---
+++ week2/Control.java
@@ -0,0 +1,13 @@
+package com.labs.week2;
+
+public class Control
+{
+	public static void main(String[] args)
+	{
+		Animal spot=new Animal("Spot","Dalmation",4,true,"Black and white",4);
+		Animal leo=new Animal("Leo");
+
+		System.out.println(spot);
+		System.out.println(leo);
+	}
+}
