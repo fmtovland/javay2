1db4a6fe082203d55009a28c74496c7152dbc792
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-01 11:14:02 +0000

    added Animal.java

---
+++ week2/Animal.java
@@ -0,0 +1,17 @@
+package com.labs.week2;
+
+public class Animal
+{
+
+	String name;
+	String breed;
+	int age;
+	boolean domesticAnimal;
+	String colour;
+
+	public Animal(String name)
+	{
+		this.name=name;
+	}
+
+}
