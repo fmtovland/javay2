f1bc6685e27a1a91a768c0b3d4192b3dabeeea7e
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-01 11:32:37 +0000

    animal.java has reached it's present specifications, but has yet to be tested

--- week2/Animal.java
+++ week2/Animal.java
@@ -14,4 +14,33 @@ public class Animal
 		this.name=name;
 	}
 
+	public Animal(String name, String breed, int age, boolean domestic, String colour)
+	{
+		this.name=name;
+		this.breed=breed;
+		this.age=age;
+		this.domesticAnimal=domestic;
+		this.colour=colour;
+	}
+
+	public String toString()
+	{
+		String returnable;
+
+		returnable="This animal is called "+name+", ";		//add name to string
+		returnable=returnable+"is a "+breed+" that is "+colour+" in colour. ";
+		returnable=returnable+"he is "+Integer.toString(age)+" years old. ";
+		if(domesticAnimal)
+		{
+			returnable=returnable+"He is a domestic animal.";
+		}
+
+		else
+		{
+			returnable=returnable+"He is not a domestic animal.";
+		}
+
+		return returnable;
+
+	}
 }
