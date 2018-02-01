097a3e549a7a0da5dd828ff28fbb4851cd29fe93
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-01 13:23:52 +0000

    added class headers for everything

--- week2/Animal.java
+++ week2/Animal.java
@@ -1,3 +1,12 @@
+/*
+Class Description: Describe an animal
+Author: Liam McCormick
+Date: 1-2-18
+OS: Arch
+Kernel: 4.14.15-1-ARCH
+Java version: 1.8.0_162
+*/
+
 package com.labs.week2;
 
 public class Animal
