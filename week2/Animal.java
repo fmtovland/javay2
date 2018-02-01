3fadccb066b9dc3740f13bd57c1b059f52debd1d
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-01 11:38:42 +0000

    added number of legs to class animal

--- week2/Animal.java
+++ week2/Animal.java
@@ -8,19 +8,21 @@ public class Animal
 	int age;
 	boolean domesticAnimal;
 	String colour;
+	int legs;
 
 	public Animal(String name)
 	{
 		this.name=name;
 	}
 
-	public Animal(String name, String breed, int age, boolean domestic, String colour)
+	public Animal(String name, String breed, int age, boolean domestic, String colour,int legs)
 	{
 		this.name=name;
 		this.breed=breed;
 		this.age=age;
 		this.domesticAnimal=domestic;
 		this.colour=colour;
+		this.legs=legs;
 	}
 
 	public String toString()
@@ -28,17 +30,19 @@ public class Animal
 		String returnable;
 
 		returnable="This animal is called "+name+", ";		//add name to string
-		returnable=returnable+"is a "+breed+" that is "+colour+" in colour. ";
-		returnable=returnable+"he is "+Integer.toString(age)+" years old. ";
-		if(domesticAnimal)
+		returnable=returnable+"is a "+breed+" that is "+colour+" in colour. ";	//add breed and colour
+		returnable=returnable+"he is "+Integer.toString(age)+" years old. ";	//add age
+		if(domesticAnimal)		//add domestic or not
 		{
 			returnable=returnable+"He is a domestic animal.";
 		}
 
 		else
 		{
-			returnable=returnable+"He is not a domestic animal.";
+			returnable=returnable+"He is not a domestic animal. ";
 		}
+
+		returnable=returnable+"He has "+Integer.toString(legs)+" legs.";	//add number of legs
 
 		return returnable;
 
