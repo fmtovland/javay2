f60fd271052a6c072be85563838ccaea26dbf043
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-08 11:46:10 +0000

    added tostring caller to control

---
+++ week3/Control.java
@@ -0,0 +1,19 @@
+/*
+Class Description: control class
+Author: Liam Mc Cormick
+Date: 8-2-18
+OS:arch
+kernel: 4.14.15-1-ARCH
+java version: 1.8.0_162
+*/
+
+package com.labs.week3;
+
+class Control
+{
+	public static void main(String[] args)
+	{
+		Employee martha=new Employee("Martha","Jones",8,999.99);
+		System.out.println(martha);
+	}
+}
