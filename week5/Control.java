c75f87a46c10f654eeb0b37a11cb18ee74ed6ed8
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-21 12:59:18 +0000

    added a class to instanciate basicscreen

---
+++ week5/Control.java
@@ -0,0 +1,12 @@
+//instanciate the basicscreen class
+
+package com.labs.week5;
+
+class Control
+{
+	public static void main(String args[])
+	{
+		BasicScreen myscreen=new BasicScreen("screen");
+
+	}
+}
