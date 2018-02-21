97c38b2c51f8b96b935973f497da79293a94a19c
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-21 13:10:10 +0000

    added more gui elements to to basicscreen

--- week5/BasicScreen.java
+++ week5/BasicScreen.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JCheckBox;
 
 
 public class BasicScreen extends JFrame implements ActionListener
@@ -12,15 +13,24 @@ public class BasicScreen extends JFrame implements ActionListener
 	JButton button1;
 	JButton button2;
 
+	JCheckBox checkbox;
+
 	BasicScreen(String title)
 	{
 		super(title);
-		setSize(300,300);
+		setSize(1280,720);	//change window default size
 		setLayout(new FlowLayout());
+
 		button1=new JButton("Click me");
+		button2=new JButton("No click me instead");
 		add(button1);
+		add(button2);
 		button1.addActionListener(this);
-		setVisible(true);
+
+		checkbox=new JCheckBox("ignore them?");
+		add(checkbox);
+
+		setVisible(true);	//forget this and the window wont appear
 	}
 
 	public void actionPerformed(ActionEvent event)
