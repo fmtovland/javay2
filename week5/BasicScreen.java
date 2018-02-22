aee4ce8da023b4aedcf4186ea91e02cff946e0dc
Liam Mc Cormick <ljmc2000@gmail.com>
2018-02-22 11:56:12 +0000

    added comments and implemented mouselistener

--- week5/BasicScreen.java
+++ week5/BasicScreen.java
@@ -3,14 +3,18 @@ package com.labs.week5;
 import java.awt.FlowLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.JPanel;
 
-public class BasicScreen extends JFrame implements ActionListener
+
+public class BasicScreen extends JFrame implements MouseListener, ActionListener
 {
 	JButton button1;
 	JButton button2;
@@ -20,12 +24,15 @@ public class BasicScreen extends JFrame implements ActionListener
 	JTextField box;
 	JButton buttonOk;	//confirm button for text box
 
+	JPanel mypanel;
+
 	BasicScreen(String title)
 	{
 		super(title);
 		setSize(1280,720);	//change window default size
 		setLayout(new FlowLayout());
 
+		//buttons 1&2
 		button1=new JButton("Click me");
 		button2=new JButton("No click me instead");
 		add(button1);
@@ -33,15 +40,22 @@ public class BasicScreen extends JFrame implements ActionListener
 		button1.addActionListener(this);
 		button2.addActionListener(this);
 
+		//the checkbox
 		checkbox=new JCheckBox("ignore them?");
 		add(checkbox);
 
+		//the text field
 		JTextField box= new JTextField("Name");
 		buttonOk=new JButton("Ok");
 		box.addActionListener(this);
 		buttonOk.addActionListener(this);
 		add(box);
 		add(buttonOk);
+
+		//jpanel
+		mypanel=new JPanel();
+		mypanel.addMouseListener(this);
+		add(mypanel);
 
 		setVisible(true);	//forget this and the window wont appear
 	}
@@ -66,4 +80,27 @@ public class BasicScreen extends JFrame implements ActionListener
 		}
 
 	}
+
+
+	//implements MouseListener
+	public void mousePressed(MouseEvent event2)
+	{
+	}
+
+	public void mouseReleased(MouseEvent event3)
+	{
+	}
+
+	public void mouseEntered(MouseEvent event4)
+	{
+	}
+
+	public void mouseExited(MouseEvent event5)
+	{
+	}
+
+	public void mouseClicked(MouseEvent event6)
+	{
+	}
+
 }
