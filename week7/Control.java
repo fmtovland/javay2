7606f3e85541e2be007a3c103acca9c76f58bc75
Liam Mc Cormick <ljmc2000@gmail.com>
2018-03-08 11:21:36 +0000

    copied Control.java from last lab

---
+++ week7/Control.java
@@ -0,0 +1,12 @@
+//instanciate the basicscreen class
+
+package com.labs.week7;
+
+class Control
+{
+	public static void main(String args[])
+	{
+		BasicScreen myscreen=new BasicScreen("screen");
+
+	}
+}
