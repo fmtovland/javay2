65ca53ea68ac6b9606ec2130f9e19a7086ea0f75
Liam Mc Cormick <ljmc2000@gmail.com>
2018-03-14 15:55:57 +0000

    Stan lee is now a Nurse

--- week8/test.java
+++ week8/test.java
@@ -7,7 +7,7 @@ class test
 	public static void main(String[] args)
 	{
 		Person p1=new Person("John","Smith",new Date(1,1,1970),"male");
-		Employee e1=new Employee("Stan","Lee",new Date(28,12,1922),"male",new Job("artist", 1000000),new Date(15,1,1939));
+		Employee e1=new Employee("Stan","Lee",new Date(28,12,1922),"male",new Job("Nurse", 1000000),new Date(15,1,1939));
 
 		System.out.println(p1+"\n");
 		System.out.println(e1);
