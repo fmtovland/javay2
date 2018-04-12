37649145e3579660cca5ac244738717776db5275
Liam Mc Cormick <ljmc2000@gmail.com>
2018-04-12 11:29:42 +0100

    my first hashset

---
+++ week10/TestHashSet.java
@@ -0,0 +1,10 @@
+package com.labs.week10;
+import java.util.HashSet;
+import java.util.Arrays;
+
+class TestHashSet
+{
+	int i;
+	String[] euroCons={"Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark",  "Estonia",  "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxemburg", "Macedonia", "Malta", "Moldova", "Monaco", "Montenegro", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia",  		"Spain", "Sweden", "Switzerland", "the Netherlands", "the United Kingdom", "Turkey", "Ukraine"};
+	HashSet europe=new HashSet<String>(Arrays.asList(euroCons));
+}
