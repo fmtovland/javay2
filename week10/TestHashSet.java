a3c2758e2f12ab08d84020b53e01627974f3b8e5
Liam Mc Cormick <ljmc2000@gmail.com>
2018-04-12 11:38:47 +0100

    added printset and addentry to testhashset

--- week10/TestHashSet.java
+++ week10/TestHashSet.java
@@ -1,5 +1,6 @@
 package com.labs.week10;
 import java.util.HashSet;
+import java.util.Iterator;
 import java.util.Arrays;
 
 class TestHashSet
@@ -7,4 +8,18 @@ class TestHashSet
 	int i;
 	String[] euroCons={"Albania", "Andorra", "Austria", "Belarus", "Belgium", "Bosnia Herzegovina", "Bulgaria", "Croatia", "Cyprus", "Czech Republic", "Denmark",  "Estonia",  "Hungary", "Iceland", "Ireland", "Italy", "Kosovo", "Latvia", "Liechtenstein", "Lithuania", "Luxemburg", "Macedonia", "Malta", "Moldova", "Monaco", "Montenegro", "Norway", "Poland", "Portugal", "Romania", "Russia", "San Marino", "Serbia", "Slovakia", "Slovenia",  		"Spain", "Sweden", "Switzerland", "the Netherlands", "the United Kingdom", "Turkey", "Ukraine"};
 	HashSet europe=new HashSet<String>(Arrays.asList(euroCons));
+
+	public void printset()
+	{
+		Iterator ittr=europe.iterator();
+		while(ittr.hasNext())
+		{
+			System.out.println(ittr.next());
+		}
+	}
+
+	void addEntry (String country)
+	{
+		HashSet.add(country);
+	}
 }
